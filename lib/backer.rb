41bc769ba02204e62a5dd4fd3ac217d35a2f8010
Joey Luis Echevarria <jechevarria719@gmail.com>
2019-09-22 16:22:20 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,13 +1,14 @@
 class Backer
   attr_reader :name, :backed_projects
-  
+
   def initialize(name)
-    @name = name 
+    @name = name
     @backed_projects = []
-  end 
-  
-  def backed_project(project)
-    backed_project << project 
-    project.backers << self 
   end
+
+  def back_project(project)
+    backed_projects << project
+    project.backers << self
+  end
+
 end
